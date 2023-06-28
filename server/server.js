721e6d5a36276b7dff1d6cd5c6b6954b78572891
Franco Nieva <franconieva86@gmail.com>
2023-06-28 10:19:37 +0200

    added cookie-parser middleware

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ const app = express();
 const path = require('path');
 const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+const cookieParsr = require('cookie-parser');
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+app.use(express.json());
+
+app.use(cookieParsr());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
