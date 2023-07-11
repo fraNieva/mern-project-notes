0ac687ab4f349eef03988894a7de118c4ece6cda
Franco Nieva <franconieva86@gmail.com>
2023-07-11 13:39:29 +0200

    added username in welcome

--- client/src/features/auth/Welcome.js
+++ client/src/features/auth/Welcome.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const Welcome = () => {
+	const { username } = useAuth();
 	const date = new Date();
 	const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(
 		date
@@ -10,7 +12,7 @@ const Welcome = () => {
 		<section className="welcome">
 			<p>{today}</p>
 
-			<h1>Welcome!</h1>
+			<h1>Welcome {username}!</h1>
 
 			<p>
 				<Link to="/dash/notes">View techNotes</Link>
